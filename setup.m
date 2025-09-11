7f14a60991e3a516d388e6c402727f6aeafbd428
almaz <ali89.05@gmail.com>
2025-09-10 22:44:57 -0700

    change: removing setup file

--- setup.m
+++
@@ -1,9 +0,0 @@
-% Add this repo's folders to the MATLAB path
-customization_repo_root = fileparts(mfilename("fullpath"));
-addpath(genpath(customization_repo_root));
-
-% Refresh Simulink to recognize the  customizations.
-sl_refresh_customizations;
-
-
-disp([newline '[+] Simulink Customization setup complete.']);
