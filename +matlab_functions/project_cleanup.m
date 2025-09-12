054a768fe2e3dc774f9bc9686953603af8728950
almaz <ali89.05@gmail.com>
2025-09-12 11:56:45 -0700

    add: function to check for py3 processes and if full_clean, removed detected processes

--- +matlab_functions/project_cleanup.m
+++ +matlab_functions/project_cleanup.m
@@ -1,3 +1,17 @@
-function project_cleanup(~)
+function project_cleanup(full_clean)
+
+   
+    [python_detected, python_processes_count] = chk_py3();
+
+    if python_detected && full_clean
+        [status, ~] = system('taskkill /f /im python3.exe');
+        
+     % if status is non-zero integer, the command failed
+         if status
+             error('[-] Unable to terminate python processes');
+         else
+            disp(['[+] PYTHON LOCK CHECK: Terminated python processes: ' num2str(python_processes_count)]); 
+         end 
+    end
 
 end
